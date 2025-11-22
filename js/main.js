48a8121eac7882403ee12ad2155eba77ce199687
Bryant Facenda <bryantffacen@gmail.com>
2025-11-21 22:39:43 -0400

    añadidos archivos index.html y main.js
    desarrollo de las primeras características del código clase padre y sus respectivas clases hijas

---
+++ js/main.js
@@ -0,0 +1,108 @@
+/**
+ * Mini Proyecto
+ *
+ * Crear un juego en JS que se ejecute completamente en consola y
+ * que cumpla con las siguientes características:
+ *
+ * El juego debe simular una auto batalla entre múltiples personajes.
+ *
+ * Para la creación de los personajes, se deberá crear una clase
+ * padre la cual cuente con las siguientes propiedades:
+ * - hp: Valor numérico que representa la vida de los personajes.
+ * - atk: Valor numérico que representa el poder de ataque.
+ * - def: Valor numérico que representa el poder de defensa.
+ * - spd: Valor numérico que representa la velocidad del personaje.
+ *
+ * Nota: Todos los valores (salvo hp) deberán ser un valor
+ * aleatorio entre 1 y un valor máximo definido por el desarrollador.
+ *
+ * Además de una serie de clases hijas (mínimo 3) que representen
+ * a cada tipo de personaje (Ej.: Guerrero, Mago, Arquero, etc.).
+ *
+ * Cada clase hija deberá heredar las propiedades del padre y poseer
+ * habilidades (mínimo 3) únicas que puedan ser utilizadas durante
+ * la batalla.
+ *
+ * El flujo del juego debe ser el siguiente:
+ * 1. Crear múltiples personajes de diferentes clases y almacenarlos
+ *    en un array.
+ * 2. Iniciar la batalla donde los personajes se enfrenten entre sí
+ *    de forma automática. El orden de ataque debe basarse en la
+ *    propiedad spd (velocidad) de cada personaje.
+ * 3. Durante cada turno, un personaje debe atacar a otro
+ *    seleccionado al azar. El daño infligido debe calcularse usando
+ *    las propiedades atk y def de los personajes involucrados;
+ *    así como una de las habilidades disponibles del atacante
+ *    seleccionada al azar.
+ * 4. Si un personaje pierde toda su hp, debe ser eliminado del
+ *    juego.
+ * 5. La batalla continúa hasta que solo quede un personaje en pie,
+ *    quien será declarado el ganador.
+ *
+ * El juego debe mostrar en consola el progreso de la batalla,
+ * incluyendo los ataques realizados, el daño infligido, los hp
+ * restantes de cada personaje y finalmente el ganador.
+ *
+ * Fecha: 26/11/2025;
+ */
+
+class Personaje {
+  constructor(hp, atk, def, spd) {
+    this.hp = hp;
+    this.atk = atk;
+    this.def = def;
+    this.spd = spd;
+  }
+}
+
+class Guerrero extends Personaje {
+  constructor(hp, atk, def, spd) {
+    super(hp, atk, def, spd);
+    this.habilidades = [
+      {
+        nombre: "Corte",
+        descripcion: "Corta a su objetivo con su espada",
+        daño: 10,
+      },
+      {
+        nombre: "Golpe de espada",
+        descripcion: "Golpea a su objetivo con su espada",
+        daño: 15,
+      },
+      {
+        nombre: "Ataque triple",
+        descripcion: "Ataque triple a su objetivo",
+        daño: 20,
+      },
+    ];
+  }
+  atacar(objetivo) {
+    const habilidad =
+      this.habilidades[Math.floor(Math.random() * this.habilidades.length)];
+    objetivo.hp -= habilidad.daño;
+    console.log(
+      `${this.nombre} ataca a ${objetivo.nombre} con ${habilidad.nombre} y le inflige ${habilidad.daño} de daño. HP restante: ${objetivo.hp}`
+    );
+  }
+}
+
+class Mago extends Personaje {
+  constructor(hp, atk, def, spd) {
+    super(hp, atk, def, spd);
+  }
+}
+
+class Arquero extends Personaje {
+  constructor(hp, atk, def, spd) {
+    super(hp, atk, def, spd);
+  }
+}
+
+class Brujo extends Personaje {
+  constructor(hp, atk, def, spd) {
+    super(hp, atk, def, spd);
+  }
+}
+
+const guerrero = new Guerrero(100, 10, 5, 8);
+console.log(guerrero);
